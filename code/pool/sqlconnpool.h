749d68d396239da620888b7208762cf8eabaeb2a
shunshi1 <GR1851116736@outlook.com>
2023-08-03 13:35:06 +0800

    提交项目所有代码

---
+++ code/pool/sqlconnpool.h
@@ -0,0 +1,43 @@
+/*
+ * @Author       : mark
+ * @Date         : 2020-06-16
+ * @copyleft Apache 2.0
+ */ 
+#ifndef SQLCONNPOOL_H
+#define SQLCONNPOOL_H
+
+#include <mysql/mysql.h>
+#include <string>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include <thread>
+#include "../log/log.h"
+
+class SqlConnPool {
+public:
+    static SqlConnPool *Instance();
+
+    MYSQL *GetConn();
+    void FreeConn(MYSQL * conn);  // 释放数据库连接，放到池子
+    int GetFreeConnCount();
+
+    void Init(const char* host, int port,
+              const char* user,const char* pwd, 
+              const char* dbName, int connSize);
+    void ClosePool();
+
+private:
+    SqlConnPool();
+    ~SqlConnPool();
+
+    int MAX_CONN_;  // 最大的连接数
+    int useCount_;  // 当前的用户数
+    int freeCount_; // 空闲的用户数
+
+    std::queue<MYSQL *> connQue_;
+    std::mutex mtx_;  // 互斥锁
+    sem_t semId_;  // 信号量
+};
+
+#endif // SQLCONNPOOL_H
